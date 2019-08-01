de560e8e48efb107e6762c5754f5d926aa319d21
brenborbs <borbonbrennon@gmail.com>
2019-08-01 10:12:40 +0800

    Added hooks API example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import RecordList from "./RecordApp/RecordList";
 import Users from "./Hooks/users";
+import HooksMovie from "./Hooks-API/hooksApi";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Users} />
         <Route path="/recordList" exact component={RecordList} />
+        <Route path="/hooksMovie" exact component={HooksMovie} />
       </Switch>
     </div>
   );
